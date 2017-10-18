5e181f862c85297dcbe2daeea20f47a2a5dca6c4
Hack Reactor Student <student@hackreactor.com>
2017-10-18 16:17:39 -0700

    board.js helpers implemented

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,30 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      //acquire the row using rowIndex
+      var row = this.get(rowIndex);
+      //try to isolate all of the pieces of there are any (hint: hofs are bae)
+      var countPieces = row.filter(square => square === 1);
+      //if there are more than one rook/queen in the row then return true
+      return countPieces.length > 1;
+      //anything else is fake news 
+      //no edge cases (rows are already made and rowindex is auto-generated)
+
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      //get the size of the board (this will be your constraint when iteration through it)
+      var size = this.get('n');
+      //iterate through the board and use the previously written helper function
+      for (var i = 0; i < size; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      //you will use indexes 0 to (n - 1) to iterate through the rows
+      //any instance of a true being returned means there is a conflict so return true
+      //finishing the loop is grounds for this function being fake news
       return false; // fixme
     },
 
@@ -94,11 +113,33 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      //get boardsize
+      var n = this.get('n');
+      //declare the array you will push your col variables to
+      var columnPieces = [];
+      //make a loop that iterates through all the column elements (hint: try this.get(rowIndex)[colIndex])
+      for (var i = 0; i < n; i++) {
+        if (this.get(i)[colIndex] === 1) {
+          columnPieces.push(1);
+        }
+      }
+      //only iterate through rows (keep colIndex the same)
+      //push elements to new array
+      //use new array like it's a row to isolate the pieces
+      return columnPieces.length > 1;
+      //use conditional to evaluate the length with respect to the conditions for a conflict
+    
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      //literally the same as the row counterpart, but with namechange
+      var size = this.get('n');
+      for (var i = 0; i < size; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -109,11 +150,33 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //the usual: board size and diag array
+      var n = this.get('n');
+      var diagonal = [];
+      //feel free to rename this arbitrarily long name 
+      var col = majorDiagonalColumnIndexAtFirstRow;
+      //use a loop like it's col, but this time instead of arr[i][i] iteration it's arr[i][i + colIndex]
+      for (var i = 0; i < n; i++) {
+        diagonal.push(this.get(i)[i + col]);
+      }
+      //same concepts in pushing element like in col
+      diagonal = diagonal.filter(element => element === 1);
+      //evaluate it, you got this
+      return diagonal.length > 1; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      //same copypasta as relevant functions, but with a twist!!!
+      var n = this.get('n');
+      for (var i = -n + 1; i < n; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      //think about how to address the elements at the lower left
+      //aka try to think about why earlier we saw in the console [undef, undef, undef, 0]
+      //one small tweak homie 
       return false; // fixme
     },
 
@@ -124,11 +187,29 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //very very similar to the major counterpart, but it decrements instead
+      //gl;hf
+      var diagonal = [];
+      var n = this.get('n');
+      var col = minorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < n; i++) {
+        diagonal.push(this.get(i)[col - i]);
+      } 
+      diagonal = diagonal.filter(element => element === 1);
+      return diagonal.length > 1; // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
+      //exactly the opposite of its major counterpart (especially the decrementing)
+      //its lit fam
+      var n = this.get('n');
+      for (var i = 2 * n - 1; i >= 0; i--) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      
       return false; // fixme
     }
 
