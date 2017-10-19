43af9ab1e905a48c70da809b11b6ac924d4c4839
Hack Reactor Student <student@hackreactor.com>
2017-10-19 11:28:39 -0700

    nqueens complete

--- src/solvers.js
+++ src/solvers.js
@@ -30,12 +30,41 @@ var factorial = function(n) {
 };
 
 window.findNRooksSolution = function(n) {
-  var solution = makeEmptyMatrix(n); //fixme
-  //creates a new board
-  //the loop is sort of a boilerplate;
-  for (var i = 0; i < n; i++) {
-    solution[i][i] = 1;
-  }
+  var solution = undefined; //fixme
+  //declare a flag that would indicate a solution being found
+  var flag = false;
+  //initialize a new board
+  var board = new Board({n: n});
+  //closure function that uses the index
+  var placeRook = function(index = 0) {
+    if (index === n) {
+      flag = true;
+      return;
+    }
+    for (let i = 0; i < n; i++) {
+      board.togglePiece(index, i);
+      if (!board.hasAnyRooksConflicts()) {
+        placeRook(index + 1);
+      }
+      if (flag === true) {
+        return;
+      }
+      board.togglePiece(index, i);
+    }
+  };
+  
+  
+  //inside closure:
+  //base case (think about making the flag true)
+  //loop + recursion (think about toggle and checking for any conflicts)
+  
+  placeRook();
+  solution = board.rows();
+  
+  
+  
+  //have solution reference the board's arrays (hint hint board.rows())
+  
   
   
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
@@ -59,35 +88,29 @@ window.findNQueensSolution = function(n) {
   console.log(board.rows());
   var placeQueen = function(index = 0) {
     if (index === n) { 
-      return true; 
+      solved = true;
+      return; 
     }
-    
-    var row = board.get(index);
     
     for (var i = 0; i < n; i++) {
 
       board.togglePiece(index, i);
       
       if (!board.hasAnyQueensConflicts()) {
-        solved = placeQueen(index + 1);
-      } else {
-        board.togglePiece(index, i);
-      }
+        placeQueen(index + 1);
+      } 
       
       if (solved) {
-        return true;
+        return;
       }
       
+      board.togglePiece(index, i);
+      
     }
-  
-    return false;
   };
-  
-  solved = placeQueen();
+  placeQueen();
   //can't find a way to access array, so a loop is called to transfer everything in board to solution;
-  for (let i = 0; i < n; i++) {
-    solution[i] = board.get(i);
-  }
+  solution = board.rows();
   
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   
@@ -96,8 +119,39 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
+  var solutionCount = 0;
+  var board = new Board({n: n});
+  
+  var placeQueen = function(index = 0) {
+    if (index === n) {
+      solutionCount++;
+      return;
+    }
+    
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(index, i);
+      
+      if (!board.hasAnyQueensConflicts()) {
+        placeQueen(index + 1);
+      }
+      
+      board.togglePiece(index, i);
+    }
+  };
+  
+  placeQueen();
+  
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
+
+
+
+
+
+
+
+
